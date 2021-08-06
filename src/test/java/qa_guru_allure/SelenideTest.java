70e00b3822ff2aee3dacbfea2c86fa32c0b31a08
usatenko <usatenko_e@mail.ru>
2021-08-06 23:49:09 +0300

    Added Allure

---
+++ src/test/java/qa_guru_allure/SelenideTest.java
@@ -0,0 +1,29 @@
+package qa_guru_allure;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+
+    @Test
+    public void repositoryIssueTest() {
+        //SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(byText("#68")).should(Condition.exist);
+    }
+
+}
